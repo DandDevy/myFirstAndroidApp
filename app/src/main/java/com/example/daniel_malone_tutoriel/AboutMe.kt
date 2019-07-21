d2e4b28411f830c303e46563255a6f856240c935
Daniel Ashcroft <dashcroft4@gmail.com>
2019-07-21 17:29:03 +0100

    made an android app that has a button to another activity.

---
+++ app/src/main/java/com/example/daniel_malone_tutoriel/AboutMe.kt
@@ -0,0 +1,13 @@
+package com.example.daniel_malone_tutoriel
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AboutMe : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_me)
+    }
+}
